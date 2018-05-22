fe9c74535806ff96ebab53d5e21ccb9dc0051027
chenzhq <chenzheqi.czq@163.com>
2018-05-22 20:27:00 +0800

    feat: 添加网站favicon

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -68,6 +68,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         process.env.NODE_ENV === 'testing' ? 'index.html' : config.build.index,
       template: 'index.html',
       inject: true,
+      favicon: path.resolve('./static/favicon.ico'),
       minify: {
         removeComments: true,
         collapseWhitespace: true,
