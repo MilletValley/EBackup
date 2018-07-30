ca09df6808e768523e28c357e7837adc39b0c17c
qian-sy <qian-sy@github.com>
2018-07-30 15:12:36 +0800

    fix: 修改404和500样式

--- src/components/pages/NoFound.vue
+++ src/components/pages/NoFound.vue
@@ -5,8 +5,8 @@
                 <img src = "../../assets/NoFound.svg" style="margin-top: 60px;"/>
             </div>
             <div style = "position: absolute; top: 20px; right: 10%;">
-                <h1 style = "font-size:72px; height: 10px; padding-top: -20px;">404</h1>
-                <p>抱歉，你访问的页面不存在</p>
+                <h1 style = "font-size:72px; margin-top: 80px; padding-top: -20px;">404</h1>
+                <p style="margin-top: 50px;">抱歉，你访问的页面不存在</p>
                 <el-button type="primary" style = "padding: 10px 15px; margin-top: 5px;"
                            @click="$router.push({ path: '/dashboard'})">返回首页</el-button>
             </div>
