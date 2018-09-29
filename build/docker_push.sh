9cc573bd3470e85c636c2dfec57bb55cbadcf610
MilletValley <937043316@qq.com>
2018-09-29 11:32:20 +0800

    fix

--- build/docker_push.sh
+++ build/docker_push.sh
@@ -8,5 +8,6 @@ then
   docker push gw920529/ebackup:latest
 elif [ $branch = "devolop-9.29" ]
 then
+  docker tag gw920529/ebackup:dev-latest gw920529/ebackup:dev-9.29-latest
   docker push gw920529/ebackup:dev-9.29-latest
 fi
