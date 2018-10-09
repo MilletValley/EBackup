9a0a270402a96d98309df7d064eedd2ccf004917
MilletValley <937043316@qq.com>
2018-10-09 08:59:47 +0800

    fix

--- build/docker_push.sh
+++ build/docker_push.sh
@@ -2,12 +2,8 @@
 branch=$1
 if [ $branch = "develop" ]
 then
-  docker push gw920529/ebackup:dev-latest
+  docker push qiansy/ebackup:dev-latest
 elif [ $branch = "master" ]
 then
-  docker push gw920529/ebackup:latest
-elif [ $branch = "devolop-9.29" ]
-then
-  docker tag gw920529/ebackup:dev-latest gw920529/ebackup:dev-9.29-latest
-  docker push gw920529/ebackup:dev-9.29-latest
+  docker push qiansy/ebackup:latest
 fi
