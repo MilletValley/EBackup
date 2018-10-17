0578fb2b2cc9d2f335e1fdbc733b08279da02f76
MilletValley <937043316@qq.com>
2018-10-17 10:37:48 +0800

    fix：自动构建配置修改

--- build/docker_push.sh
+++ build/docker_push.sh
@@ -2,14 +2,16 @@
 branch=$1
 if [ $branch = "develop" ]
 then
-  docker push qiansy/ebackup:dev-latest
+  docker push gw920529/ebackup:dev-latest
 elif [ $branch = "master" ]
 then
-  docker push qiansy/ebackup:latest
-elif [ $branch = "disaster-recover" ]
+  docker push gw920529/ebackup:latest
+elif [ $branch = "devolop-9.29" ]
 then
-  docker push qiansy/ebackup:disaster-recover
+  docker tag gw920529/ebackup:dev-latest gw920529/ebackup:dev-9.29-latest
+  docker push gw920529/ebackup:dev-9.29-latest
 elif [ $branch = "version2.3" ]
 then
-  docker push qiansy/ebackup:version2.3
+  docker tag gw920529/ebackup:dev-latest gw920529/ebackup:v2.3-latest
+  docker push gw920529/ebackup:v2.3-latest
 fi
