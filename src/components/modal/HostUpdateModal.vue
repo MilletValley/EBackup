6833deda098b69b4c97e9aae146f36ff02c1ea21
MilletValley <937043316@qq.com>
2018-11-13 12:00:19 +0800

    fix：添加设备时添加windowtype字段

--- src/components/modal/HostUpdateModal.vue
+++ src/components/modal/HostUpdateModal.vue
@@ -164,9 +164,6 @@ export default {
       this.$refs.itemUpdateForm.validate(valid => {
         if (valid) {
           let data = {...this.formData};
-          if(!(data.osName === 'Windows' && data.databaseType === 2)){
-            delete data.windowsType
-          }
           this.$emit('confirm', data);
         } else {
           return false;
