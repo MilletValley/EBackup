dc59cd4ee69b8be30cad5cea3119661352753f18
MilletValley <937043316@qq.com>
2018-11-13 10:21:17 +0800

    fix：删除未及时刷新问题

--- src/components/modal/VmBackupTable.vue
+++ src/components/modal/VmBackupTable.vue
@@ -81,8 +81,9 @@
 </template>
 <script>
 import {
-    deleteVirtualBackupPlan,
-  getVmsBackupResult} from '../../api/virtuals';
+  deleteVirtualBackupPlan,
+  getVmsBackupResult,
+} from '../../api/virtuals';
 import {
   backupStrategyMapping,
   timeStrategyMapping,
@@ -90,102 +91,109 @@ import {
   operationStateMapping,
 } from '../../utils/constant';
 import baseMixin from '../mixins/baseMixins';
-import {paginationMixin, filterMixin, sortMixin} from '../mixins/commonMixin';
+import { paginationMixin, filterMixin, sortMixin } from '../mixins/commonMixin';
 export default {
-    mixins: [baseMixin, paginationMixin, filterMixin, sortMixin],
-    props:{
-        id: {
-            type: Number
-        },
-        tableData: {
-            type: Array,
-            default: () => []
-        }
+  mixins: [baseMixin, paginationMixin, filterMixin, sortMixin],
+  props: {
+    id: {
+      type: Number,
     },
-    data(){
-        return {
-            loading: false,
-            timer: null,
-            inputSearch: '',
-            defaultSort:{prop: 'startTime', order: 'descending'},
-        }
+    tableData: {
+      type: Array,
+      default: () => [],
     },
-    mounted(){
-        // this.fetchAll();
-        // this.setTimer(this.timer);
+  },
+  data() {
+    return {
+      loading: false,
+      timer: null,
+      inputSearch: '',
+      defaultSort: { prop: 'startTime', order: 'descending' },
+    };
+  },
+  mounted() {
+    // this.fetchAll();
+    // this.setTimer(this.timer);
+  },
+  destroyed() {
+    // this.clearTimer(this.timer);
+  },
+  methods: {
+    fetchAll() {
+      this.loading = true;
+      getVmsBackupResult(this.id)
+        .then(res => {
+          this.tableData = res.data.data;
+        })
+        .catch(error => {
+          this.$message.error(error);
+        })
+        .then(() => {
+          this.loading = false;
+        });
     },
-    destroyed() {
-        // this.clearTimer(this.timer);
+    formatProcess(data) {
+      if (data.state === 1) {
+        return data.processSpeed;
+      } else if (data.state === 0) {
+        return 0;
+      } else if (data.state === 2) {
+        return 100;
+      } else if (data.state === 3) {
+        return data.processSpeed;
+      }
     },
-    methods: {
-        fetchAll(){
-            this.loading = true
-            getVmsBackupResult(this.id).then( res => {
-                // this.tableData = res.data.data;
-            }).catch( error => {
-                this.$message.error(error);
-            }).then( () => {
-                this.loading = false
+    formatState(state) {
+      if (state === 2) {
+        return 'success';
+      } else if (state === 3) {
+        return 'exception';
+      }
+      return '';
+    },
+    // setTimer() {
+    //     this.clearTimer();
+    //     this.timer = setInterval(() => {
+    //         getVmsBackupResult(this.id).then( res => {
+    //             // this.tableData = res.data.data;
+    //         })
+    //     }, 10000);
+    // },
+    // clearTimer() {
+    //     clearInterval(this.timer);
+    // },
+    deletePlan(scope) {
+      this.$confirm('请确认是否删除？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      })
+        .then(() => {
+          deleteVirtualBackupPlan(scope.row.id)
+            .then(res => {
+              this.$message.success('删除成功');
+              // this.fetchAll();
+              this.tableData.splice(
+                this.tableData.findIndex(item => item.id === scope.row.id),
+                1
+              );
             })
-        },
-        formatProcess(data){
-            if(data.state === 1){
-                return data.processSpeed;
-            }else if (data.state === 0){
-                return 0;
-            }else if (data.state === 2){
-                return 100
-            }else if (data.state === 3){
-                return data.processSpeed;
-            }
-        },
-        formatState(state){
-            if(state === 2){
-                return 'success';
-            }else if( state === 3){
-                return 'exception'
-            }
-            return ''
-        },
-        // setTimer() {
-        //     this.clearTimer();
-        //     this.timer = setInterval(() => {
-        //         getVmsBackupResult(this.id).then( res => {
-        //             // this.tableData = res.data.data;
-        //         })
-        //     }, 10000);
-        // },
-        // clearTimer() {
-        //     clearInterval(this.timer);
-        // },
-        deletePlan(scope){
-            this.$confirm(
-                '请确认是否删除？',
-                '提示',
-                {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }
-            ).then(() => {
-                deleteVirtualBackupPlan(scope.row.id).then( res => {
-                    this.$message.success( '删除成功');
-                    this.fetchAll();
-                }).catch( error => {
-                    this.$message.error( error);
-                });
-            }).catch( () => {});
-        },
-        filterFn(item, i){
-            return item.vm[i].includes( this.filter[i]);
-        },
-        searchByName(){
-            const vmName = this.inputSearch
-            this.filter = Object.assign({},{vmName});
-            this.currentPage = 1;
-        }
-    }
-}
+            .catch(error => {
+              this.$message.error(error);
+            });
+        })
+        .catch(() => {});
+    },
+    filterFn(item, i) {
+      return item.vm[i].includes(this.filter[i]);
+    },
+    searchByName() {
+      const vmName = this.inputSearch;
+      this.filter = Object.assign({}, { vmName });
+      this.currentPage = 1;
+    },
+  },
+};
 </script>
 <style lang="scss" module>
 @import '../../style/common.scss';
