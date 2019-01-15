2e1fe1d41cbcabbb671c5f67dbeffb1ce153f754
MilletValley <937043316@qq.com>
2019-01-15 14:14:25 +0800

    fix：

--- src/components/pages/file/FileHostDetail.vue
+++ src/components/pages/file/FileHostDetail.vue
@@ -362,10 +362,10 @@ export default {
     // 删除备份计划
     deleteBackupPlan(planId) {
       deleteBackupPlan(planId).then(() => {
-        this.backupPlans.splice(
-          this.backupPlans.findIndex(plan => plan.id === planId),
-          1
-        );
+        // this.backupPlans.splice(
+        //   this.backupPlans.findIndex(plan => plan.id === planId),
+        //   1
+        // );
         this.$message.success('删除成功');
       }).catch(error => {
         this.$message.error(error);
@@ -375,10 +375,10 @@ export default {
     deleteRestorePlan(planId) {
       deleteSingleRestorePlan(planId)
         .then(() => {
-          this.restorePlans.splice(
-            this.restorePlans.findIndex(plan => plan.id === planId),
-            1
-          );
+          // this.restorePlans.splice(
+          //   this.restorePlans.findIndex(plan => plan.id === planId),
+          //   1
+          // );
           this.$message.success('删除成功');
         })
         .catch(error => {
