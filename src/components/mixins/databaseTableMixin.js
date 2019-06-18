daaa1d90af0196d4598959350db5ae4d3acff7d3
MilletValley <937043316@qq.com>
2019-06-18 09:19:05 +0800

    fix：回退

--- src/components/mixins/databaseTableMixin.js
+++ src/components/mixins/databaseTableMixin.js
@@ -26,11 +26,6 @@ const tableMixin = {
       objectType: ''
     };
   },
-  computed: {
-    monitorConf() {
-      return this.$store.state.nav.monitorconf;
-    }
-  },
   mounted() {
     this.fetchData();
   },
