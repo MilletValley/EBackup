e5d3dc290ce2530377c05d9533313e7faad230e1
qian-sy <2535354643@qq.com>
2019-07-29 18:26:32 +0800

    fix: 修改bug

--- src/components/pages/inspection/InspectionTask.vue
+++ src/components/pages/inspection/InspectionTask.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <iframe :src="`http://localhost:8089/inspect/ebackup/inspection`"
+    <iframe :src="`http://${inspectWeb.ip}:${inspectWeb.port}/inspect/ebackup/inspection`"
             width="100%"
             height="800"
             frameborder="0"
